343f8d26374de14f310b04263e01d34f8edfa5fe
Kye-Lee <kyelee.low@gmail.com>
2025-02-21 13:55:51 +0000

    imported <ComponentTemplate> to view component in app on browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import ComponentTemplate from './components/component-template/ComponentTemplate';
 
 function App() {
   return (
     <div className="App">
-      <h1>Seneca Frontend Task</h1>
+      <ComponentTemplate />
     </div>
   );
 }
