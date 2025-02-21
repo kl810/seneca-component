66ebcf26987f6a84dcc22455595e7b431be4f017
Kye-Lee <kyelee.low@gmail.com>
2025-02-21 18:11:25 +0000

    created toggle-input with no text-overflow and 2 answer options, useState(isToggled, setIsToggled) and onClick{handleToggle} used to switch states(boolean) when button is clicked, .selected css applies if isToggled === true for left-button and isToggled != true for right-button

---
+++ src/components/toggle-input/ToggleInput.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import './ToggleInput.css';
+
+
+interface ToggleInputProps {
+    options: [string, string]
+}
+
+export default function ToggleInput({options}: ToggleInputProps) {
+    const [isToggled, setIsToggled] = useState(false)
+    const handleToggle = () => {
+        setIsToggled(isToggled => !isToggled);
+    };
+
+    return (
+        <>
+            <div className="toggle-container">
+                <button 
+                    type="button" 
+                    onClick={handleToggle} 
+                    className={`toggle ${isToggled ? 'selected': ''}`} //if isToggled === true, .selected css applies
+                    disabled={isToggled}                               //disable button if it is clicked/isToggled === true
+                >
+                    <h5 className="text">{options[0]}</h5> 
+                </button>
+                <button 
+                    type="button" 
+                    onClick={handleToggle} 
+                    className={`toggle ${isToggled ? '': 'selected'}`} //opposite to left-button in same container
+                    disabled={!isToggled}
+                >
+                    <h5 className="text">{options[1]}</h5>
+                </button>
+            </div>
+        </>
+    )
+}
