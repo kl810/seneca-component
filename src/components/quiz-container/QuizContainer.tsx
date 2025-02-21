9e1e8caca7377107b2751290aa6a90811e0fd643
Kye-Lee <kyelee.low@gmail.com>
2025-02-21 19:45:42 +0000

    refactored ToggleInput options prop to use options data from quizData array

--- src/components/quiz-container/QuizContainer.tsx
+++ src/components/quiz-container/QuizContainer.tsx
@@ -1,15 +1,32 @@
 import './QuizContainer.css';
 import ToggleInput from '../toggle-input/ToggleInput';
 
+const quizData = [
+    {
+        id: 1,
+        options: ["Cell Wall", "Ribosomes"],
+        answer: "Ribosomes"
+    },
+    {
+        id: 2,
+        options: ["Cytoplasm", "Chloroplast"],
+        answer: "Cytoplasm"
+    },
+    {
+        id: 3,
+        options: ["Cellulose", "Mitochondria"],
+        answer: "Mitochondria"
+    }
+]
+
 export default function QuizContainer() {
     return (
         <div className="quiz-wrapper">
             <div className="quiz-content">
                 <h4 className="question-heading">An animal cell contains:</h4>
-                <ToggleInput options={["Cell Wall", "Ribosomes"]}/>
-                <ToggleInput options={["Cytoplasm", "Choloplast"]}/>
-                <ToggleInput options={["Cell Wall", "Ribosomes"]}/>
-                <ToggleInput options={["Cellulose", "Mitochondria"]}/>
+                {quizData.map(({id, options}) => 
+                    <ToggleInput key={id} options={[options[0], options[1]]}/>)
+                }
                 <p className="answer-statement">The answer is incorrect</p>
             </div>
         </div>
