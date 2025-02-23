1cfa32ee54afab77c8a0de783e61ccb43b12a816
Kye-Lee <kyelee.low@gmail.com>
2025-02-23 14:44:52 +0000

    add correctCount

--- src/features/quiz/quizSlice.ts
+++ src/features/quiz/quizSlice.ts
@@ -4,46 +4,56 @@ export const quizSlice = createSlice({
   name: 'quiz',
   initialState: {
     question: "An animal cell contains",
-    correct: 0,
+    correctCount: 0,
     answerOptions: [
         {
             "id": 1,
             "options": ["Cell Wall", "Ribosomes"],
             "selectedAnswer": "",
             "correctAnswer": "Ribosomes",
+            "isCorrect": false,
         },
         {
             "id": 2,
             "options": ["Cytoplasm", "Chloroplast"],
             "selectedAnswer": "",
             "correctAnswer": "Cytoplasm",
+            "isCorrect": false,
         },
         {
             "id": 3,
             "options": ["Cellulose", "Mitochondria"],
             "selectedAnswer": "",
             "correctAnswer": "Mitochondria",
+            "isCorrect": false,
         },
         {
             "id": 4,
             "options": ["Cell Wall", "Ribosomes"],
             "selectedAnswer": "",
             "correctAnswer": "Ribosomes",
+            "isCorrect": false,
         }
     ]
   },
   reducers: {
     updateSelectedAnswer: (state, action) => {
       const {toggleId, selectedAnswer} = action.payload
-      const activeToggle = state.answerOptions.find(toggle => toggle.id === toggleId)
+      const activeToggle = state.answerOptions.find(toggle => toggle.id === toggleId) //find selected toggle
       if (activeToggle) {
-        activeToggle.selectedAnswer = selectedAnswer
+        activeToggle.selectedAnswer = selectedAnswer          //updated selectedAnswer with value of button clicked
+        activeToggle.isCorrect = (activeToggle.selectedAnswer === activeToggle.correctAnswer)   // check if selectedAnswer === correctAnswer and if true, set isCorrect to true        
       }
-    },
+
+      const correctCount = state.answerOptions.reduce((count, option) =>    //iterate array and count number of isCorrect is true
+        count + (option.isCorrect ? 1 : 0), 0);
+
+      state.correctCount = correctCount;     //update number of correct answers
+
+    }
   }
 })
 
 export const { updateSelectedAnswer } = quizSlice.actions
-
 
 export default quizSlice.reducer
