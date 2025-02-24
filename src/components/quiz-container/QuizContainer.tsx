432f5e0f85badd57b9a9526e01394e2e2a61d65b
Kye-Lee <kyelee.low@gmail.com>
2025-02-24 18:50:53 +0000

    added dynamic background-color

--- src/components/quiz-container/QuizContainer.tsx
+++ src/components/quiz-container/QuizContainer.tsx
@@ -4,10 +4,34 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
 
 export default function QuizContainer() {
-    const { question, answerOptions, isComplete } = useSelector((state: RootState) => state.quiz)
+    const { question, answerOptions, isComplete, correctCount } = useSelector((state: RootState) => state.quiz)
+    
+    const percentCorrect = parseFloat((correctCount / answerOptions.length).toFixed(2));
+
+    const getBgColor = (count: number, options: Array<object>): string => {
+
+        if (percentCorrect === 0) return "none-correct";
+        if (percentCorrect <= 0.3) return "few-correct";
+        if (percentCorrect <= 0.6) return "some-correct";
+        if (percentCorrect <= 0.99) return "mostly-correct";
+        if (percentCorrect === 1) return "all-correct";
+
+        return "none-correct";
+    }
 
     return (
-        <div className="quiz-wrapper">
+        <div 
+            className={"quiz-wrapper"}
+            style={{
+                background: 
+                    `${ isComplete ? 
+                        "linear-gradient(180deg, #76E0C2 0%, #59CADA 100%)" :       
+                        `linear-gradient(180deg, rgba(244,223,40,1) 0%,       
+                            rgba(244,223,40,${percentCorrect}) 80%), 
+                            rgb(238,107,45)`
+                    }`  //change background-color based on percentageLeft 
+            }}
+        >
             <div className="quiz-content">
                 <h4 className="question">{question}:</h4>
                 {answerOptions.map(({id, options}, index) => 
@@ -16,6 +40,9 @@ export default function QuizContainer() {
                         id={id}
                         options={options}
                         isComplete={isComplete}
+                        correctCount={correctCount}
+                        answerOptions={answerOptions}
+                        getBgColor= {getBgColor}
                     />)
                 } 
                 <p className="answer-statement">The answer is {isComplete ? "correct!" : "incorrect"}</p>
