b39568da75b0eeb7cb54bec95a7693354c6039bf
Kye-Lee <kyelee.low@gmail.com>
2025-02-25 21:13:48 +0000

    import testData for initialState, created goToNextQuestion reducer

--- src/features/quiz/quizSlice.ts
+++ src/features/quiz/quizSlice.ts
@@ -1,61 +1,48 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { testData } from './testInitialState'
 
 export const quizSlice = createSlice({
   name: 'quiz',
   initialState: {
-    question: "An animal cell contains",
-    correctCount: 0,
-    isComplete: false,
-    answerOptions: [
-        {
-            "id": 1,
-            "options": ["Cell Wall", "Ribosomes"],
-            "selectedAnswer": "",
-            "correctAnswer": "Ribosomes",
-            "isCorrect": false,
-        },
-        {
-            "id": 2,
-            "options": ["Cytoplasm", "Chloroplast"],
-            "selectedAnswer": "",
-            "correctAnswer": "Cytoplasm",
-            "isCorrect": false,
-        },
-        {
-            "id": 3,
-            "options": ["Cellulose", "Mitochondria"],
-            "selectedAnswer": "",
-            "correctAnswer": "Mitochondria",
-            "isCorrect": false,
-        },
-        {
-            "id": 4,
-            "options": ["Cell Wall", "Ribosomes"],
-            "selectedAnswer": "",
-            "correctAnswer": "Ribosomes",
-            "isCorrect": false,
-        }
-    ]
+    currentIndex: 0,      // Track current question index
+    quizData: testData, ...testData[0], // Store all quiz questions // Initialize state with first question
   },
   reducers: {
-    updateSelectedAnswer: (state, action) => {
+    updateSelectedAnswer: (state, action) => {          
       const {toggleId, selectedAnswer} = action.payload
-      const activeToggle = state.answerOptions.find(toggle => toggle.id === toggleId) //find selected toggle
+
+      // Refer to { testData: answerOptions }
+      const activeToggle = state.answerOptions.find(toggle => toggle.id === toggleId)           // Find selected toggle
       if (activeToggle) {
-        activeToggle.selectedAnswer = selectedAnswer          //updated selectedAnswer with value of button clicked
-        activeToggle.isCorrect = (activeToggle.selectedAnswer === activeToggle.correctAnswer)   // check if selectedAnswer === correctAnswer and if true, set isCorrect to true        
+        activeToggle.selectedAnswer = selectedAnswer                               // Updated selectedAnswer with value of button clicked
+        activeToggle.isCorrect = (activeToggle.selectedAnswer === activeToggle.correctAnswer)   // Check if selectedAnswer === correctAnswer and if true, set isCorrect to true        
       }
 
-      const correctCount = state.answerOptions.reduce((count, option) =>    //iterate array and count number of isCorrect is true
-        count + (option.isCorrect ? 1 : 0), 0);
+      state.correctCount = state.answerOptions.reduce((count, option) =>          
+        count + (option.isCorrect ? 1 : 0), 
+        0
+      );
 
-      state.correctCount = correctCount;     //update number of correct answers
+      state.isComplete = state.correctCount === state.answerOptions.length      // Check if answer is complete
 
-      state.isComplete = correctCount === state.answerOptions.length  //isComplete true when all answers correct
-    }
+      state.showNextButton = state.isComplete            // Show next button when question is complete
+    },
+    goToNextQuestion: (state) => {
+
+      if (state.currentIndex <= state.quizData.length - 1) {
+        state.currentIndex += 1;
+        Object.assign(state, state.quizData[state.currentIndex]); // Update state with next question
+        state.showNextButton = false;                            // Hide next button until next question is completed
+      }
+
+      if (state.currentIndex === state.quizData.length) {        // Check if all questions completed
+        state.allQuestionsCompleted = true;
+        state.showNextButton = true;
+      }
+    },
   }
 })
 
-export const { updateSelectedAnswer } = quizSlice.actions
+export const { updateSelectedAnswer, goToNextQuestion } = quizSlice.actions
 
 export default quizSlice.reducer
---
+++ src/features/quiz/testInitialState.ts
@@ -0,0 +1,76 @@
+export const testData = [
+    {
+        question: "An animal cell contains",
+        correctCount: 0,
+        isComplete: false,
+        showNextButton: false,
+        allQuestionsCompleted: false,
+        answerOptions: [
+            {
+                "id": 1,
+                "options": ["Cell Wall", "Ribosomes"],
+                "selectedAnswer": "",
+                "correctAnswer": "Ribosomes",
+                "isCorrect": false,
+            },
+            {
+                "id": 2,
+                "options": ["Cytoplasm", "Chloroplast"],
+                "selectedAnswer": "",
+                "correctAnswer": "Cytoplasm",
+                "isCorrect": false,
+            },
+            {
+                "id": 3,
+                "options": ["Cellulose", "Mitochondria"],
+                "selectedAnswer": "",
+                "correctAnswer": "Mitochondria",
+                "isCorrect": false,
+            },
+            {
+                "id": 4,
+                "options": ["Cell Wall", "Ribosomes"],
+                "selectedAnswer": "",
+                "correctAnswer": "Ribosomes",
+                "isCorrect": false,
+            }
+        ]
+    },
+    {
+        question: "What are the ideal conditions inside an office?",
+        correctCount: 0,
+        isComplete: false,
+        showNextButton: false,
+        allQuestionsCompleted: false,
+        answerOptions: [
+            {
+                "id": 1,
+                "options": ["Good Pay", "Bad Pay"],
+                "selectedAnswer": "",
+                "correctAnswer": "Good Pay",
+                "isCorrect": false,
+            },
+            {
+                "id": 2,
+                "options": ["Lot of meetings", "Less meetings"],
+                "selectedAnswer": "",
+                "correctAnswer": "Less meetings",
+                "isCorrect": false,
+            },
+            {
+                "id": 3,
+                "options": ["Free Coffee", "Expensive Coffee"],
+                "selectedAnswer": "",
+                "correctAnswer": "Free Coffee",
+                "isCorrect": false,
+            },
+            {
+                "id": 4,
+                "options": ["Bear in office", "Dog in office"],
+                "selectedAnswer": "",
+                "correctAnswer": "Dog in office",
+                "isCorrect": false,
+            }
+        ]
+    }
+]
